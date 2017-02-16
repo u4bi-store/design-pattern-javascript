69404e56399df073bc9c1f8ac02c823ed6188c94
u4bi <u4bi@u4bi.com>
2017-02-16 18:02:44 +0900

    creational pattern - Object.defineProperty

--- javascript/js/creational_patterns.js
+++ javascript/js/creational_patterns.js
@@ -10,18 +10,27 @@ obj['d'] = '네번째 디';
 var tempA = obj['a'];
 var tempB = obj.b;
 
-Object.defineProperty(obj, 'e', {
-  value : 'u4bi_e'
-});
+/* https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+   
+   객체 속성 정의
+   Object.defineProperty(obj, prop, descriptor)
+   obj : 속성의 정의하고자 하는 객체
+   prop : 새로 정의하거나 수정하려는 속성의 이름임
+   desc : prop에 대해서 정의하려는 객체
+*/
 
-Object.defineProperties(obj, {
-  f : {
-    value : 'u4bi_f'
-  },
-  g : {
-    value : 'u4bi_g'
+Object.defineProperty(obj, 'e', {
+    value : '다섯번째 이'
   }
-});
+);
+
+/* 아래 형식으로도 정의 가능 객체로 넣고 내부 속성 정의함*/
+Object.defineProperties(obj,
+  {
+    f : { value : '여섯번째 에프' },
+    g : { value : '일곱번째 쥐' }
+  }
+);
 
 console.log('템프 에이에 담긴 값 : '+tempA);
 console.log('템프 비에 담긴 값 : '+tempB);
