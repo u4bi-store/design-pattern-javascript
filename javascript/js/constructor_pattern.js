e0eedf73d666f2ca4ab3b6945c1c858b51a20c76
u4bi <u4bi@u4bi.com>
2017-02-16 18:33:11 +0900

    Creational Patterns : constructor pattern

--- javascript/js/constructor_pattern.js
+++ javascript/js/constructor_pattern.js
@@ -1,19 +1,23 @@
-function Person(firstName, lastName){
-  this.firstName = firstName;
-  this.lastName = lastName;
+function Person(firstName, lastName){ /* 펄슨이란 객체의 인자를 두개로 주고*/
+  this.firstName = firstName; /* 내부 변수값에 인자를 지정함 */
+  this.lastName = lastName; /* 내부 변수값에 인자를 지정함 */
   
-  // this.sayName = function(){
-  //   return '내 이름은 '+this.firstName+' '+this.lastName;
-  // };
-  /* 1번 */
+  this.sayName = function(){ /* 컨테이너 편션 지정 */
+    return '내 이름은 '+this.firstName+' '+this.lastName;
+    /* 내부에 지정한 값들을 반환함 */
+  };
+  /* 1번 방법 */
 }
 
-Person.prototype.sayName = function(){
-  return '내 이름은 '+ this.firstName+' '+this.lastName;
-};
-/* 2번 */
+/* 주석처리 위의 객체 내부에 선언을 함 */
+// Person.prototype.sayName = function(){
+//   return '내 이름은 '+ this.firstName+' '+this.lastName;
+// };
+/* 2번 방법 */
 
+/* 명재라는 변수에 펄슨이란 객체를 선언함 데이터 타입 지정함 */
 var Myungjae = new Person('명재', '유');
+/* 철수라는 변수에 펄슨이란 객체를 선언함 데이터 타입 지정함 */
 var Chulsoo = new Person('철수', '유');
 
 var isPerson = Myungjae instanceof Person; // true
