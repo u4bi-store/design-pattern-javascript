6ad43943ec3253587342318352bfaf6b4a30d1af
u4bi <u4bi@u4bi.com>
2017-02-16 18:47:31 +0900

    Creational Patterns : constructor pattern prototype

--- javascript/js/constructor_pattern.js
+++ javascript/js/constructor_pattern.js
@@ -2,17 +2,19 @@ function Person(firstName, lastName){ /* 펄슨이란 객체의 인자를 두개
   this.firstName = firstName; /* 내부 변수값에 인자를 지정함 */
   this.lastName = lastName; /* 내부 변수값에 인자를 지정함 */
   
-  this.sayName = function(){ /* 컨테이너 편션 지정 */
-    return '내 이름은 '+this.firstName+' '+this.lastName;
-    /* 내부에 지정한 값들을 반환함 */
-  };
+  /* 프로토타입으로 지정위해 주석처리 */
+  // this.sayName = function(){ /* 컨테이너 편션 지정 */
+  //   return '내 이름은 '+this.firstName+' '+this.lastName;
+  //   /* 내부에 지정한 값들을 반환함 */
+  // };
   /* 1번 방법 */
 }
 
-/* 주석처리 위의 객체 내부에 선언을 함 */
-// Person.prototype.sayName = function(){
-//   return '내 이름은 '+ this.firstName+' '+this.lastName;
-// };
+/* 펄슨의 프로토타입을 지정할 수 있음
+   컨테이너 펀션 지정 내부에 지정한 값들을 동일하게 반환함 */
+Person.prototype.sayName = function(){
+  return '내 이름은 '+ this.firstName+' '+this.lastName;
+};
 /* 2번 방법 */
 
 /* 명재라는 변수에 펄슨이란 객체를 선언함 데이터 타입 지정함 */
