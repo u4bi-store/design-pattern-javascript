206b41a9b96029b9d28d93f3008aded730e405a9
u4bi <u4bi@u4bi.com>
2017-02-16 20:21:35 +0900

    code reuse patterns : mixins person, dog function called

--- javascript/js/mixins.js
+++ javascript/js/mixins.js
@@ -9,20 +9,24 @@ function extend(target){
   }
 }
 
-function Person(name){
+function Person(name){ /* 펄슨 지정 */
   this.name = name;
 }
 
-function Dog(name){
+function Dog(name){ /* 도그 지정*/
   this.name = name;
 }
 
+/* 스피커라는 객체를 지정 */
 var speaker = {
   speak: function(){
     return this.name + '가 말한다.';
   }
 };
 
+/* 무버라는 객체를 지정함
+   그안에는 워크와 런이 정의되어 있음
+*/
 var mover = {
   walk: function(){
     return this.name + '가 걷는다.';
@@ -38,16 +42,30 @@ var worker = {
   }
 };
 
-extend(Person.prototype, speaker, mover, worker);
-extend(Dog.prototype, speaker, mover);
+/* 제이쿼리에 ectend와 동일한 구조라 함 
+   $.extend(Person.prototype, speaker, mover)
+   $.extend(Person.prototype, speaker, mover)
+   
+   제이쿼리에서는 이러한 형식으로 사용됨
+   하지만 믹스인 패턴을 직접 만들꺼임
+*/
 
-var Myungjae = new Person('유명재');
-var Popo = new Dog('포포');
+/* 잠시 주석 */
+// extend(Person.prototype, speaker, mover, worker);
+// extend(Dog.prototype, speaker, mover);
+
+var Myungjae = new Person('유명재'); /* 객체를 생성하여 선언함 펄슨의 이름은 유명재로 */
+var Popo = new Dog('포포'); /* 객체를 생성하여 선언함 도그의 이름은 포포 */
+
+/* 현재는 위에 정의된 speaker와 mover가 믹스인 되지 않았음
+   즉 아래 콘솔을 찍어봤을 때 오류가 날꺼임
+*/
 
 
 console.log(Myungjae.walk()); /* 유명재가 걷는다. */
 console.log(Myungjae.work()); /* 유명재가 공부한다. */
 console.log(Popo.run()); /* 포포가 뛴다. */
 console.log(Popo.speak()); /* 포포가 말한다. */
+/* 오류 확인 */
 
 
