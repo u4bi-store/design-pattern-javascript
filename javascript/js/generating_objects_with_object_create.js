0c4a3b3765bb631a4e393ef20861bca92cfa847c
u4bi <u4bi@u4bi.com>
2017-02-16 18:17:28 +0900

    Creational patterns : generating object -  Object.create

--- javascript/js/generating_objects_with_object_create.js
+++ javascript/js/generating_objects_with_object_create.js
@@ -1,19 +1,33 @@
 // var foo = {};
 // var bar = Object.create(Object.prototype);
 
-var Myungjae = {
-  firstName : '명재',
+var Myungjae = { /* 명재라는 객체 선언 */
+  firstName : '명재', /* 속성 정의*/
   lastName: '유',
-  sayName : function(){
+  sayName : function(){ /* 컨테이너 펀션 정의 위 두값 리턴 */
     return '내 이름은 '+ this.firstName+' '+this.lastName;
   }
 };
 
+/*
+  https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Object/create
+  
+  지정한 객체 속성을 갖는 새 객체를 만듬
+  Object.create(proto[, propertiesObject])
+  proto : 새로 만든 객체의 프로토타입 이어야할 객체
+  propertiesObject : 선택사항임
+    - 새로 만든 객체에 추가되거나 수정할 속성을 지정할 수 있음.
+    이는 Objdect.defineProperties()의 두번째 인수에 해당하기도 함.
+*/
+
+/* 또 아래는 철수라는 객체 선언
+   명재라는 객체로 지정함
+   명재의 객체 속성을 가짐 */
 var Chulsoo = Object.create(Myungjae, {
-  firstName : {value : '철수'},
-  greet: {
-    value : function(person){
-      return '안녕! '+person.firstName;
+  firstName : {value : '철수'}, /* 철수라는 이름으로 지정함 즉 유철수가 됨 */
+  greet: { /* 새롭게 속성을 추가함 */
+    value : function(person){ /* 이의 값을 컨테이너 펀션으로 지정하고 */
+      return '오? 안녕! '+person.firstName; /* 해당 펀션의 인자로 받은 객체에게 정의 내려진 firestName을 반환함 */
     }
   }
 });
