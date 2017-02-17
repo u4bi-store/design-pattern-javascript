693e167b3717e66415b051707ec106538c4e41a8
u4bi <u4bi@u4bi.com>
2017-02-17 11:39:57 +0900

    other structural patterns : command pattern - array push obj this return function undo

--- command_pattern.js
+++ command_pattern.js
@@ -1,5 +1,6 @@
 function Calculator(){ /* 칼큘레이터 펀션 */
-    this._currentValue = 0; /* 현재값 */   
+    this._currentValue = 0; /* 현재값 */
+    this.commands = [];   
 }
 
 Calculator.prototype = { /* 프로토타입 지정 */
@@ -11,15 +12,33 @@ Calculator.prototype = { /* 프로토타입 지정 */
             인자로 들어간 객체로 반환을 하고 로직이 구성되는데
             구성된 후의 반환이 된 값은 this. 현재값으로 정의됨
         */
+        this.commands.push(command); /* 객체를 담음 */
+    },
+    undo : function(){ /* 언도 펀션을 둠 */
+        var cmd = this.commands.pop();
+        /* https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/pop
+           
+           배열에서 마지막 요소를 제거하고 그 제거한 요소를 반환함
+           
+           arr.pop();
+
+           즉 배열의 마지막 요소를 제거하고
+           그 값을 호출자에게 반홤함
+        */
+        this._currentValue = cmd.undo(this._currentValue);
+        /* 반환한 해당 객체의 this시점내 _currentValue를
+           현재 _currentValue에 지정함 */
     },
     getCurrentValue : function(){ /* 현재 객체에 정의된 현재값을 가져온다 */
         return this._currentValue; /* this.현재값이 반환됨 */
     }
 }
 
-function Command(fn, value){ 
+function Command(fn, undo, value){ 
     this.execute = fn; /* 현 시점의 객체주소를 익스큐트로 지정함 */
-    this.value = value; /* 두번째 인자를 벨류에 넣음*/
+    this.undo = undo; /* 두번째 인자로 언도가 들어가짐 */
+    this.value = value; /* 세번째 인자를 벨류에 넣음*/
+    
 }
 
 /* https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Function/call
@@ -30,16 +49,21 @@ function Command(fn, value){
    thisArg      : 펀션 호출에 제공되는 this값
    arg1, arg2.. : 객체를 위한 인수
 */
+
+function add(value){
+    return value + this.value; /* this시점에 로직이 구성된 후 반환 this시점의 인자(즉 _currentValue) + 현재 인자값*/
+}
+
+function sub(value){ /* 익명 펀션으로 로직구성 */
+    return value - this.value; /* 로직이 구성된 후 반환 this시점의 인자(즉 _currentValue) - 현재 인자값*/
+}
+
 function AddCommend(value){ /* 에드 커맨드 */
-    Command.call(this, function(value){
-        return value + this.value; /* this시점에 로직이 구성된 후 반환 this시점의 인자(즉 _currentValue) + 현재 인자값*/
-    }, value); /* 현재 인자값 정의 */
+    Command.call(this, add/*익명 펀션밖으로 뺌*/,sub/*언도에 담길 값*/, value); /* 현재 인자값 정의 */
 }
 
 function SubCommend(value){ /* 섭 커맨드 */
-    Command.call(this, function(value){ /* 익명 펀션으로 로직구성 */
-        return value - this.value; /* 로직이 구성된 후 반환 this시점의 인자(즉 _currentValue) - 현재 인자값*/
-    }, value); /* 현재 인자값 정의 */
+    Command.call(this, sub/*익명 펀션밖으로 뺌*/,add/*언도에 담길 값*/, value); /* 현재 인자값 정의 */
 }
 
 var calc = new Calculator(); /* calc에 객체 선언 */
@@ -73,3 +97,23 @@ console.log(calc.getCurrentValue());
 /* 현재의 _currentValue의 값을 확인한다.
    19에서 4가 빠진 15이다.
 */
+
+calc.undo(); /* 언도를 하게될 시
+  해당 객체의 언도 펀션이 호출되면서
+  마지막 배열에 담겨진 객체를 반환하는데
+  반환된 객체의 언도 펀션이 호출되는데
+  인자로 this시점 내_currentValue를 가져간다.
+
+  이 언도 펀션은 그 반환된 객체내의(Command) 두번째 담겨진 인자 펀션으로
+  그에 해당되는 로직이 구성되어 값을 반환하는데
+  
+  이 값이 현재 calc객체의 _currentValue로 지정이 된다.
+*/
+
+console.log(calc.getCurrentValue());
+/* 현재의 _currentValue의 값을 확인한다.
+   15에서 언도펀션 로직이 돌아가 이전 객체의 값인 19가 된다. */
+
+calc.undo(); /* 만약에 다시 한번 언도를 할 시 */
+console.log(calc.getCurrentValue());
+/* 0이 된 것을 볼 수 있다. */
