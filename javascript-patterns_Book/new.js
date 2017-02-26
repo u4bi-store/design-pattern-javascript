2af45afbf904af7645386c595215c9391c9231e1
u4bi <u4bi@u4bi.com>
2017-02-26 17:08:50 +0000

    new instanceof

--- javascript-patterns_Book/new.js
+++ javascript-patterns_Book/new.js
@@ -36,3 +36,117 @@ console.log(tastes); /* yummy (전역 네임스페이스에 접근되어 버림)
    이런 동작 방식은 바람직하지 않다고 함.
    
 */
+
+
+/* 이를 위한 대안
+   
+   명명규칙
+   가장 간단한 대안은 앞장에서 다룬 명명 규칙을 사용하는 것임.
+   생성자 함수명의 첫글자를 대문자로 쓰는 것임.
+   
+   일반적인 함수와 메서드의 첫글자는 소문자로 사용
+   
+   that 사용
+   명명 귳칙을 따르는 것도 꽤 도움이 되지만 이는 올바른 동작 방식을 권고할 뿐
+   강제하지는 못함.
+   항상 생성자로 동작하도록 해주는 패턴이 있음.
+   
+   this에 모든 멤버를 추가하는 대신
+   that에 모든 멤버를 추가한 후 that을 반환하는 것임.
+*/
+
+/* 예로 아래 코드 */
+function WaffleB(){
+ var that = {};
+ that.tastes = 'yummy';
+ return that;
+}
+
+/*
+  간단한 객체라면 that이라는 지역 변수를 만들 필요도 없이
+  객체 리터럴을 통해 객체를 반환해도 된다고 함.
+
+  function Waffle(){
+    return {
+      tastes : 'yummy'
+    };
+  }
+
+*/
+
+/* 아래 코드의 WaffleB() 구현 중 어느것을 사용해도
+   호출 방법과 상관없이 항상 객체가 반환된다고 함.
+*/
+var waffle_A = new WaffleB(),
+    waffle_B = WaffleB();
+console.log(waffle_A.tastes); /* yummy */
+console.log(waffle_B.tastes); /* yummy */
+
+/* 이 패턴의 문제는 프로토타입과의 연결고리를 잃어버리게 된다는 점이라고 함.
+   즉 Waffle() 프로토타입에 추가한 멤버를
+   객체에서 사용할 수 없다고 함.
+   
+   that이라는 변수명은 관습적인 것으로 어떤 이름이라도 쓸 수 있다고 함.
+   흔히 사용되는 변수명으로 self와 me 등이 있다고 함.
+   
+*/
+
+
+/* 앞서 설명한 패턴의 문제점을 해결하고 인스턴스 객체에서
+   프로토타입의 프로퍼티들을 사용할 수 있게 하려면
+   아래와 같은 접근 방법을 고려해야 한다고 함.
+   
+   생성자 내부에서 this가 해당 생성자의 인스턴스인지를 확인하고
+   그렇지 않은 경우 new와 함께 스스로를 재 호출하는 방식이라고 함.
+*/
+
+function WaffleC(){
+  if(!(this instanceof WaffleC)){
+    return new WaffleC();
+  }
+  
+  this.tastes = 'yummy';
+}
+WaffleC.prototype.wantAnother = true;
+
+var waf_A = new WaffleC(),
+    waf_B = WaffleC();
+
+console.log(waf_A.tastes); /* yummy */
+console.log(waf_B.tastes); /* yummy */
+console.log(waf_A.wantAnother); /* true */
+console.log(waf_B.wantAnother); /* true */
+
+/* 인스턴스를 판별하는 또다른 범용적인 방법은 
+   
+   생성자 이름을 하드코딩하는 대신
+   arguments.callee와 비교하는 것이라고함.
+   
+   if(!(this instanceof arguments.callee)){
+      return new arguments.callee();
+   }   
+*/
+
+function WaffleD(){
+  if(!(this instanceof arguments.callee)){
+    return new arguments.callee();
+  }
+  
+  this.tastes = 'yummy D';
+}
+WaffleD.prototype.wantAnother = true;
+
+var waffleD = new WaffleD();
+console.log(waffleD.tastes); /* yummy D*/
+console.log(waffleD.wantAnother); /* true */
+
+/*
+   이것은 모든 함수가 호출 될 때
+   내부적으로 arguments라는 객체가 생성된다고 함.
+   
+   이 객체가 함수에 전달된 모든 인자를 담고 있다는 점을
+   활용한 패턴이라고 함.
+   
+   arguments의 callee라는 프로퍼티는
+   호출된 함수를 가리킨다고 함.
+*/
